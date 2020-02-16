10b6090bb02e174f01aa59d479bee4d104e22488
tonyfang <q710370@gmail.com>
2020-02-16 21:57:21 +0800

    add port

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const handlebars = require('express-handlebars')
-
-app.engine('handlebars', handlebars())
+const passport = require('./config/passport')
+const db = require('./models')
+const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+app.use('/upload', express.static(__dirname + '/upload'))
+app.use(methodOverride('_method'))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.engine('handlebars', handlebars({ defaultlayout: 'main' }))
 app.set('view engine', 'handlebars')
-
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user
+  next()
+})
 app.listen(port, () => {
   console.log(`this app is running on port: ${port}`)
 })
+
+require('./routes')(app, passport)
