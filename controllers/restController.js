55afe357ee0bffc7737cc1e555400b6e6a60b29e
tonyfang <q710370@gmail.com>
2020-02-18 19:13:45 +0800

    Add categories navbar and filter

--- controllers/restController.js
+++ controllers/restController.js
@@ -3,13 +3,24 @@ const Restaurant = db.Restaurant
 const Category = db.Category
 let restController = {
   getRestaurants: (req, res) => {
-    Restaurant.findAll({ include: Category }).then(restaurants => {
+    let whereQuery = {}
+    let categoryId = ''
+    if (req.query.categoryId) {
+      categoryId = Number(req.query.categoryId)
+      whereQuery['CategoryId'] = categoryId
+    }
+    Restaurant.findAll({ include: Category, where: whereQuery }).then(restaurants => {
       const data = restaurants.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50)
       }))
-      return res.render('restaurants', {
-        restaurants: JSON.parse(JSON.stringify(data))
+
+      Category.findAll().then(categories => {
+        return res.render('restaurants', {
+          restaurants: JSON.parse(JSON.stringify(data)),
+          categories: JSON.parse(JSON.stringify(categories)),
+          categoryId: JSON.parse(JSON.stringify(categoryId))
+        })
       })
     })
   },
