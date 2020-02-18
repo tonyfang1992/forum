4392ee85ee0c1c0894076ebab02da7eb80241ee7
tonyfang <q710370@gmail.com>
2020-02-18 17:01:57 +0800

    Add restaurants show page

--- controllers/restController.js
+++ controllers/restController.js
@@ -12,6 +12,15 @@ let restController = {
         restaurants: JSON.parse(JSON.stringify(data))
       })
     })
+  },
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category
+    }).then(restaurant => {
+      return res.render('restaurant', {
+        restaurant: JSON.parse(JSON.stringify(restaurant))
+      })
+    })
   }
 }
 module.exports = restController
