a680cf89718eb8549703605948323f2ad5bad71d
tonyfang <q710370@gmail.com>
2020-02-25 20:20:43 +0800

    PUT api/admin/restaurant/:id

--- routes/apis.js
+++ routes/apis.js
@@ -9,6 +9,7 @@ const categoryController = require('../controllers/api/categoryController.js')
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurant/:id', adminController.getRestaurant)
 router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
+router.put('/admin/restaurants/:id', upload.single('image'), adminController.putRestaurant)
 
 
 router.get('/admin/categories', categoryController.getCategories)
--- services/adminService.js
+++ services/adminService.js
@@ -51,6 +51,51 @@ const adminController = {
       })
     }
   },
+  putRestaurant: (req, res, callback) => {
+    if (!req.body.name) {
+      return callback({
+        status: 'error', message: "name didn't exist"
+      })
+    }
+
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID);
+      imgur.upload(file.path, (err, img) => {
+        return Restaurant.findByPk(req.params.id)
+          .then((restaurant) => {
+            restaurant.update({
+              name: req.body.name,
+              tel: req.body.tel,
+              address: req.body.address,
+              opening_hours: req.body.opening_hours,
+              description: req.body.description,
+              image: file ? img.data.link : restaurant.image,
+              CategoryId: req.body.categoryId
+            })
+              .then((restaurant) => {
+                callback({ status: 'success', message: 'restaurant was successfully update' })
+              })
+          })
+      })
+    }
+    else
+      return Restaurant.findByPk(req.params.id)
+        .then((restaurant) => {
+          restaurant.update({
+            name: req.body.name,
+            tel: req.body.tel,
+            address: req.body.address,
+            opening_hours: req.body.opening_hours,
+            description: req.body.description,
+            CategoryId: req.body.categoryId,
+            image: restaurant.image
+          })
+            .then((restaurant) => {
+              callback({ status: 'success', message: 'restaurant was successfully update' })
+            })
+        })
+  },
   deleteRestaurant: (req, res, callback) => {
     return Restaurant.findByPk(req.params.id)
       .then((restaurant) => {
