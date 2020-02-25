22740e37b3009212aef2380666474e6b8bc59ff5
tonyfang <q710370@gmail.com>
2020-02-25 16:40:03 +0800

    POST/api/admin/restaurants

--- routes/apis.js
+++ routes/apis.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
 
 const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController.js')
 
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurant/:id', adminController.getRestaurant)
+router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
 
 
 router.get('/admin/categories', categoryController.getCategories)
--- services/adminService.js
+++ services/adminService.js
@@ -1,6 +1,9 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+
 const adminController = {
   getRestaurants: (req, res, callback) => {
     return Restaurant.findAll({ include: [Category] }).then(restaurants => {
@@ -11,6 +14,42 @@ const adminController = {
     return Restaurant.findByPk(req.params.id, { include: [Category] }).then(restaurant => {
       callback({ restaurant: JSON.parse(JSON.stringify(restaurant)) })
     })
+  },
+  postRestaurant: (req, res, callback) => {
+    if (!req.body.name) {
+      return callback({ status: 'error', message: "name didn't exist" })
+    }
+
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID);
+      imgur.upload(file.path, (err, img) => {
+        return Restaurant.create({
+          name: req.body.name,
+          tel: req.body.tel,
+          address: req.body.address,
+          opening_hours: req.body.opening_hours,
+          description: req.body.description,
+          image: file ? img.data.link : null,
+          CategoryId: req.body.categoryId
+        }).then((restaurant) => {
+          callback({ status: 'success', message: 'restaurant was successfully created' })
+        })
+      })
+    }
+    else {
+      return Restaurant.create({
+        name: req.body.name,
+        tel: req.body.tel,
+        address: req.body.address,
+        opening_hours: req.body.opening_hours,
+        description: req.body.description,
+        CategoryId: req.body.categoryId,
+        image: null
+      }).then((restaurant) => {
+        callback({ status: 'success', message: 'restaurant was successfully created' })
+      })
+    }
   },
   deleteRestaurant: (req, res, callback) => {
     return Restaurant.findByPk(req.params.id)
